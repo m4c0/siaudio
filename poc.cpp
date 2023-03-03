a335806c99f1b5efb7bd992e73d2a83d4b33fdd9
Eduardo Costa <m4c0@users.noreply.github.com>
2023-03-03 17:37:15 -0300

    avoid clicks and clacks

--- poc.cpp
+++ poc.cpp
@@ -3,9 +3,10 @@ import siaudio;
 int main() {
   volatile auto gens = 0;
   auto s = siaudio::streamer{[&](float *data, unsigned samples) {
+    float mult = (gens < 1000) ? gens / 1000.0f : 1.0f;
     gens = gens + samples;
     for (unsigned i = 0; i < samples; i++)
-      data[i] = ((i / 20) % 2) - 0.5f;
+      data[i] = mult * ((i / 20) % 2) - 0.5f;
   }};
   while (gens < 44100) {
   }
