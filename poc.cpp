1e8c85913cd644a82f16e70259a50b62a05fba6a
Eduardo Costa <m4c0@users.noreply.github.com>
2023-03-04 13:03:22 -0300

    split main to allow reuse by wasm

--- poc.cpp
+++ poc.cpp
@@ -1,13 +1,17 @@
 import siaudio;
 
-int main() {
-  volatile auto gens = 0;
-  auto s = siaudio::streamer{[&](float *data, unsigned samples) {
+extern "C" unsigned blip() {
+  static volatile unsigned gens = 0;
+  static auto s = siaudio::streamer{[&](float *data, unsigned samples) {
     float mult = (gens < 1000) ? gens / 1000.0f : 1.0f;
     gens = gens + samples;
     for (unsigned i = 0; i < samples; i++)
       data[i] = mult * ((i / 20) % 2) - 0.5f;
   }};
-  while (gens < 44100) {
+  return gens;
+}
+
+int main() {
+  while (blip() < 44100) {
   }
 }
