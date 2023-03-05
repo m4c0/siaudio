359367a3af9511f4c5ca7ab62ab03a1f3dd426c4
Eduardo Costa <m4c0@users.noreply.github.com>
2023-03-05 09:20:32 -0300

    wasm: cleanup + some click reduction

--- poc.cpp
+++ poc.cpp
@@ -4,9 +4,10 @@ extern "C" unsigned blip() {
   static volatile unsigned gens = 0;
   static auto s = siaudio::streamer{[&](float *data, unsigned samples) {
     float mult = (gens < 1000) ? gens / 1000.0f : 1.0f;
-    gens = gens + samples;
-    for (unsigned i = 0; i < samples; i++)
-      data[i] = mult * ((i / 20) % 2) - 0.5f;
+    for (unsigned i = 0; i < samples; ++i) {
+      data[i] = mult * ((gens / 20) % 2) - 0.5f;
+      gens = gens + 1;
+    }
   }};
   return gens;
 }
--- siaudio_start.js
+++ siaudio_start.js
@@ -1,39 +1,28 @@
 function (pimpl) {
-  var audioContext;
+  const AudioContext = window.AudioContext || window.webkitAudioContext;
+  var audioContext = new AudioContext();
 
-  function audio_init() {
-    const AudioContext = window.AudioContext || window.webkitAudioContext;
-    audioContext = new AudioContext();
-  }
+  const pimpl_size = 4 * 4;
+  const params = new Uint32Array(ecow_buffer, pimpl, 4);
+  const channels = params[1];
+  const rate = params[2];
+  const buf_size = params[3];
+  const view = new Float32Array(ecow_buffer, pimpl + pimpl_size, buf_size);
 
+  const audioBuffer = audioContext.createBuffer(channels, buf_size, rate);
   function play() {
-    if (!audioContext) audio_init();
-
     ecow_exports.siaudio_fill_buffer(pimpl);
-
-    const pimpl_size = 4 * 4;
-    const params = new Uint32Array(ecow_buffer, pimpl, 4);
-    const channels = params[1];
-    const rate = params[2];
-    const buf_size = params[3];
-
-    const audioBuffer = audioContext.createBuffer(channels, buf_size, rate);
-
-    const view = new Float32Array(ecow_buffer, pimpl + pimpl_size, buf_size);
-    audioBuffer.getChannelData(0).set(view);
+    audioBuffer.copyToChannel(view, 0);
 
     const src = audioContext.createBufferSource();
     src.buffer = audioBuffer;
     src.connect(audioContext.destination);
-    src.start();
-
     src.onended = play;
+    src.start();
   }
 
   play();
 
   // TODO: write down why this is necessary and how it impacts audio timestamps
-  document.body.onclick = () => {
-    if (audioContext != null) audioContext.resume();
-  }
+  document.body.onclick = audioContext.resume.bind(audioContext);
 }
