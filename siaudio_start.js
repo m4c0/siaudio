28d80aefc6966cb34e2b8f6749205c8c07649222
Eduardo Costa <m4c0@users.noreply.github.com>
2023-03-16 11:33:55 -0300

    wasm: refactor to init AC on click
    this seems to work flawless on Safari, but Firefox/Chrome can't produce
    a clean sound

--- siaudio_start.js
+++ siaudio_start.js
@@ -1,7 +1,4 @@
 function (pimpl) {
-  const AudioContext = window.AudioContext || window.webkitAudioContext;
-  var audioContext = new AudioContext();
-
   const pimpl_size = 4 * 4;
   const params = new Uint32Array(ecow_buffer, pimpl, 4);
   const channels = params[1];
@@ -9,20 +6,28 @@ function (pimpl) {
   const buf_size = params[3];
   const view = new Float32Array(ecow_buffer, pimpl + pimpl_size, buf_size);
 
-  const audioBuffer = audioContext.createBuffer(channels, buf_size, rate);
-  function play() {
-    ecow_exports.siaudio_fill_buffer(pimpl);
-    audioBuffer.copyToChannel(view, 0);
+  const AudioContext = window.AudioContext || window.webkitAudioContext;
+  var audioContext;
 
-    const src = audioContext.createBufferSource();
-    src.buffer = audioBuffer;
-    src.connect(audioContext.destination);
-    src.onended = play;
-    src.start();
+  // AudioContext should only be created on a user interaction
+  document.body.onclick = function() {
+    if (audioContext) return;
+
+    audioContext = new AudioContext();
+
+    const audioBuffer = audioContext.createBuffer(channels, buf_size, rate);
+
+    function play() {
+      ecow_exports.siaudio_fill_buffer(pimpl);
+      audioBuffer.copyToChannel(view, 0);
+
+      const src = audioContext.createBufferSource();
+      src.buffer = audioBuffer;
+      src.connect(audioContext.destination);
+      src.onended = play;
+      src.start();
+    }
+
+    play();
   }
-
-  play();
-
-  // TODO: write down why this is necessary and how it impacts audio timestamps
-  document.body.onclick = audioContext.resume.bind(audioContext);
 }
