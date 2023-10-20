9436729fc6e5192398bb75d95ab4cd0067db2c1e
Eduardo Costa <m4c0@users.noreply.github.com>
2023-10-20 16:03:14 +0100

    using leco for build

--- poc.cpp
+++ poc.cpp
@@ -1,6 +1,13 @@
+#pragma leco tool
 import siaudio;
 
-extern "C" unsigned __attribute__((export_name("blip"))) blip() {
+#ifdef WASM
+#define EXPORT __attribute__((export_name("blip")))
+#else
+#define EXPORT
+#endif
+
+extern "C" unsigned EXPORT blip() {
   static volatile unsigned gens = 0;
   static auto s = siaudio::streamer{[&](float *data, unsigned samples) {
     float mult = (gens < 1000) ? gens / 1000.0f : 1.0f;
--- siaudio.cppm
+++ siaudio.cppm
@@ -35,3 +35,14 @@ public:
 };
 
 } // namespace siaudio
+
+#ifdef LECO_TARGET_ANDROID
+#pragma leco add_impl android android_audio
+#pragma leco add_system_ilbrary aaudio
+#elif LECO_TARGET_APPLE
+#pragma leco add_impl apple
+#pragma leco add_framework AudioToolbox
+#elif LECO_TARGET_WINDOWS
+#pragma leco add_impl windows
+#pragma leco add_system_library ole32
+#endif
