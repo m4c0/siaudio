f7ea25aea8b1bdc88dda65eab69215a16fbbefae
Eduardo Costa <m4c0@users.noreply.github.com>
2024-05-16 13:49:41 +0100

    android: migrating whatever

--- android.cpp
+++ android.cpp
@@ -5,14 +5,14 @@ extern "C" AAudioStream *siaudio_setup_aas(siaudio::os_streamer *);
 extern "C" void siaudio_teardown_aas(AAudioStream *);
 
 namespace siaudio {
-class pimpl {
+class streamer {
   AAudioStream *m_aas{};
 
 public:
-  pimpl(os_streamer *str) : m_aas{siaudio_setup_aas(str)} {};
-  ~pimpl() { siaudio_teardown_aas(m_aas); }
+  streamer() : m_aas{siaudio_setup_aas(str)} {};
+  ~streamer() { siaudio_teardown_aas(m_aas); }
 };
 
-os_streamer::os_streamer() : m_pimpl{new pimpl{this}} {}
-os_streamer::~os_streamer() = default;
+streamer_t create(unsigned rate) { return streamer_t{new streamer{rate}}; }
+void deleter::operator()(streamer *s) { delete s; }
 } // namespace siaudio
