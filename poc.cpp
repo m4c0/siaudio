41d497351a09eae5f3b50566aa5199244e1c8235
Eduardo Costa <m4c0@users.noreply.github.com>
2024-07-21 15:00:28 +0100

    poc: lowering the volume

--- poc.cpp
+++ poc.cpp
@@ -8,13 +8,13 @@ static unsigned m_gens{};
 static void fill_buffer(float *data, unsigned samples) {
   float mult = (m_gens < 1000) ? m_gens / 1000.0f : 1.0f;
   for (unsigned i = 0; i < samples; ++i) {
-    data[i] = mult * ((m_gens / 20) % 2) - 0.5f;
+    data[i] = 0.5f * (mult * ((m_gens / 20) % 2) - 0.5f);
     m_gens = m_gens + 1;
   }
 }
 static void fill_buffer_2(float *data, unsigned samples) {
   for (unsigned i = 0; i < samples; ++i) {
-    data[i] = ((m_gens / 10) % 2) - 0.5f;
+    data[i] = 0.5f * (((m_gens / 10) % 2) - 0.5f);
     m_gens = m_gens + 1;
   }
 }
