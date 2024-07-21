59b5770ca9803edfed198640ab173916097bee6c
Eduardo Costa <m4c0@users.noreply.github.com>
2024-07-21 15:03:52 +0100

    zero-volume default function

--- impl.cpp
+++ impl.cpp
@@ -5,7 +5,10 @@ static siaudio::streamer_t &instance() {
   return i;
 }
 
-void (*siaudio::fn)(float *, unsigned) = [](float *, unsigned) {};
+void (*siaudio::fn)(float *, unsigned) = [](float *p, unsigned n) {
+  for (auto i = 0; i < n; i++)
+    *p++ = 0;
+};
 
 void siaudio::filler(void (*fn)(float *, unsigned)) { siaudio::fn = fn; }
 void siaudio::rate(unsigned r) {
